1292def79792f5812b00cc4e2343740b5e8d8ad8
Gajendra Rajput <Gajendra.rajput@tcs.com>
2020-06-23 17:12:17 +0530

    refactor: Simplify the Score Board logic

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,18 @@
 const Constants = {
   APP_TITLE: "Bowling Game",
+  SCORECARD_TITLES: [
+    "Frame 1",
+    "Frame 2",
+    "Frame 3",
+    "Frame 4",
+    "Frame 5",
+    "Frame 6",
+    "Frame 7",
+    "Frame 8",
+    "Frame 9",
+    "Frame 10",
+    "Total Score",
+  ],
 };
 
 export default Constants;
