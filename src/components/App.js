8bc871c4632061d5220aff578afa567e67ab144f
Gajendra Rajput <Gajendra.rajput@tcs.com>
2020-06-23 13:40:51 +0530

    refactor: Use constant for static text

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Constants from "../constants";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Bowling Game</h1>
+        <h1>{Constants.APP_TITLE}</h1>
       </header>
     </div>
   );
